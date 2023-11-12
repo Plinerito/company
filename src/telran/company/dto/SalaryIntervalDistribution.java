427003b4d2c01a6084c8f30738ad361b8c2d3815
Plinerito <plinerzh@gmail.com>
2023-11-12 13:19:51 +0200

    removing unrelated stuff

--- src/telran/company/dto/SalaryIntervalDistribution.java
+++
@@ -1,9 +0,0 @@
-package telran.company.dto;
-
-import java.io.Serializable;
-
-public record SalaryIntervalDistribution(int salaryFrom, int SalaryTo, long amount) 
-implements Serializable                                                  // кол-во человек получающий зарплату в опр.интервале
-{
-
-}
