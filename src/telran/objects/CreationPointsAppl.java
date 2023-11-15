04c75fb38e6c836fd7c194f8efd9444ce3e709d6
Plinerito <plinerzh@gmail.com>
2023-11-15 12:10:33 +0200

    Initial

---
+++ src/telran/objects/CreationPointsAppl.java
@@ -0,0 +1,21 @@
+package telran.objects;
+
+import java.util.*;
+// разница между record и class
+import java.io.*;
+public class CreationPointsAppl {
+
+	public static void main(String[] args) throws Exception {
+		List<Point> points = new ArrayList<>();
+        points.add(new Point(3,4));
+        points.add(new Point(5,6));
+        points.add(new Point(7,8));
+        try(ObjectOutputStream output = 
+        		new ObjectOutputStream(new FileOutputStream("points.data"))){
+            output.writeObject(points);
+	}
+        System.out.println(points);
+        }
+	}
+
+
